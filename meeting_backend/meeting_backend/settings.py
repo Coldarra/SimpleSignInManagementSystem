66fe2bf973b1bd57f10992bd57dcf75f7256e0eb
Nile_zym <33719831+Coldarra@users.noreply.github.com>
2021-03-22 14:36:28 +0800

    Update settings.py
    SECRET_KEY

--- meeting_backend/meeting_backend/settings.py
+++ meeting_backend/meeting_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-9ogz#kf5faww&$y#f#k_7u^p3kr&n@v&azpr=a^om@$qakz!@'
+SECRET_KEY = '-9ogz#kf5faww&$y#f#k_7u^p3kr&n@v&azpr=a^om@$qakz!@' # 记得改SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
